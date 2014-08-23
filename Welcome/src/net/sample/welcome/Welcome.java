71e79d96128527fd68ed20b22bc63dc0da6d1081
touch <liumingjiang2013@yahoo.co.jp>
2014-08-23 14:52:04 +0800

    test

---
+++ Welcome/src/net/sample/welcome/Welcome.java
@@ -0,0 +1,15 @@
+package net.sample.welcome;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Window;
+
+public class Welcome extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle bundle) {
+        super.onCreate(bundle);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        setContentView(new WelcomeShow(this));
+    }
+}
---
+++ Welcome/src/net/sample/welcome/WelcomeShow.java
@@ -0,0 +1,19 @@
+package net.sample.welcome;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.view.View;
+
+public class WelcomeShow extends View {
+	public WelcomeShow(Context context){
+		super(context);
+		setBackgroundColor(Color.WHITE);
+	}
+	
+	protected void onDraw(Canvas canvas){
+		canvas.drawText("Welcome Andriud World Touch!",
+				0, 12, new Paint());
+	}
+}
