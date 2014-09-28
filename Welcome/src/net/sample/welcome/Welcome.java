4f66a0e813a880aea56c9f0f40289717b48a112e
liumingjiang2013@yahoo.co.jp <xiaopihai771111>
2014-09-28 17:40:36 +0900

    2.0

--- Welcome/src/net/sample/welcome/Welcome.java
+++ Welcome/src/net/sample/welcome/Welcome.java
@@ -5,7 +5,7 @@ import android.os.Bundle;
 import android.view.Window;
 
 public class Welcome extends Activity {
-    /** Called when the branch1.1 activity is first created. */
+    /** Called when the branch1.2 activity is first created. */
     @Override
     public void onCreate(Bundle bundle) {
         super.onCreate(bundle);
